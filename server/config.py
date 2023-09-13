9ec1542675446215da9ebd8af2c90035c9ea5376
Tom Tobar <cocdetombomb@gmail.com>
2023-09-13 16:53:43 -0500

    add server

---
+++ server/config.py
@@ -0,0 +1,24 @@
+# Standard library imports
+
+# Remote library imports
+from flask import Flask
+from flask_cors import CORS
+from flask_migrate import Migrate
+from flask_restful import Api
+from flask_sqlalchemy import SQLAlchemy
+
+# Local imports
+
+# Instantiate app, set attributes
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///app.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.json.compact = False
+
+db = SQLAlchemy()
+migrate = Migrate(app, db)
+db.init_app(app)
+
+api = Api(app)
+
+CORS(app)
